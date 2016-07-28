a31838d15e0c3c075aa6c49d35f4e72be61c3cd8
曾明明 <243906727@qq.com>
2016-07-28 15:30:14 +0800

    dao restful get

--- dao/UsersDao.js
+++ dao/UsersDao.js
@@ -1,36 +1,86 @@
 var db = require('../lib/db/mysql');
-var table = 'myapp_users';
+var _ = require('lodash');
+var table = 'myapp_user';
 var dao = {
-  findOne: function(params, cb) {
-    if (typeof params === 'string' || typeof params === 'number') {
-      db.execute(
-        'SELECT * FROM ' + table + ' WHERE uid=? LIMIT 1',
-        [params],
-        cb
-      );
-    } else if (typeof params === 'object') {
-      var whereParams = [], values = [];
-      for(var key in params) {
-        whereParams.push(key+'=?');
-        values.push(params[key]);
-      }
-      db.execute(
-        'SELECT * FROM ' + table + ' WHERE ' + whereParams.join(' AND ') + 'LIMIT 1',
-        values,
-        cb
-      );
-    }
 
+  insert: function(data) {
+    var _data = this._json2insert(data);
+    db.execute(
+      'INSERT ' + table + '(' + _data.fields + ') VALUES (' + _.repeat('?', _data.values.length) + ')',
+      _data.values,
+      cb
+    );
   },
-  findAll: function() {
 
-  },
-  update: function() {
-
-  },
   delete: function() {
 
-  }
+  },
+
+  update: function(data, where, cb) {
+    var _data = this._json2update(data);
+    var _where = this._json2where(where);
+    db.execute(
+      'UPDATE ' + table + ' SET ' + _data.fields + ' WHERE ' + _where.condition,
+      _data.values.concat(_where.params),
+      cb
+    );
+  },
+
+  findOneByAttr: function(where, cb) {
+    var _where = this._json2where(where);
+    db.execute(
+      'SELECT * FROM ' + table + ' WHERE ' + _where.condition + ' LIMIT 1',
+      _where.params,
+      cb
+    );
+  },
+
+  findAllByAttr: function(where, cb) {
+    var _where = this._json2where(where);
+    db.execute(
+      'SELECT * FROM ' + table + ' WHERE ' + _where.condition,
+      _where.params,
+      cb
+    );
+  },
+
+  _json2insert: function(data) {
+    var fields = [], values = [];
+    for(var key in data) {
+      fields.push(key);
+      values.push(data[key]);
+    }
+    return {
+      fields: condition.join(','),
+      values: values
+    };
+  },
+
+  _json2update: function(data) {
+    var fields = [], values = [];
+    for(var key in data) {
+      fields.push(key+'=?');
+      values.push(data[key]);
+    }
+    return {
+      fields: fields.join(','),
+      values: values
+    };
+  },
+
+  _json2where: function(data) {
+    var condition = [], params = [];
+    for(var key in data) {
+      condition.push(key+'=?');
+      params.push(data[key]);
+    }
+    return {
+      condition: condition.join(' AND '),
+      params: params
+    };
+  },
+
+
 };
 
 module.exports = dao;
--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,25 @@ var UsersDao = require('../dao/UsersDao');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  UserDao.findAll();
+router.get('/', function(req, res) {
+  UsersDao.findAllByAttr({1:1}, function(result) {
+    res.json(result);
+  });
 });
 
+router.get(/^\/\d+?$/, function(req, res) {
+  UsersDao.findOneByAttr({1:1}, function(result) {
+    res.json(result);
+  });
+});
 
+router.param('id', function(req, res, next, id) {
+  next();
+});
+router.get('/:id', function(req, res) {
+  UsersDao.findOneByAttr({1:1}, function(result) {
+    res.json(result);
+  });
+});
 
 module.exports = router;
